01fabb33fd774574605043f5590072956a0bcd5f
Artur Dantas de Medeiros <20221101110038@ifrn.local>
2025-05-28 14:29:09 -0300

    ajeotando o app.jsx

--- tarefa01_artur_dantas_de_medeiros/src/App.jsx
+++ tarefa01_artur_dantas_de_medeiros/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import Card from './componentes/card'
 import Detalhamento from './componentes/detalhes'
 import Galeria from './componentes/galeria'
@@ -13,33 +11,58 @@ import img5 from "./assets/imagem_5.jpg"
 import img6 from "./assets/imagem_6.jpg"
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+
+  const characters = [
+    {
+      img:img1,
+      nome:"Fiorra",
+      desc:"tuche macho escritor",
+    },
+    {
+      img:img2,
+      nome:"Thresh",
+      desc:"vai inche a pança sua alma",
+    },
+   
+    {
+      img:img3,
+      nome:"fiddlesticks",
+      desc:"nao vai exitir mais cafe no mundo",
+    },
+   
+    {
+      img:img4,
+      nome:"jhin",
+      desc:"vair sofr proo 4rão",
+    },
+   
+    {
+      img:img5,
+      nome:"syndra",
+      desc:"vai tomar tanta bolada que quando ver ta morto",
+    },
+   
+    {
+      img:img6,
+      nome:"mel",
+      desc:"nao da dano se reflete suas abilidades",
+    },
+   
+   
+   
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      
       <Detalhamento img={img1} nome={"Fiorra"} desc={"tuche macho escritor"} />
       <Detalhamento img={img2} nome={"Thresh"} desc={"vai inche a pança sua alma"} />
       <Detalhamento img={img3} nome={"fiddlesticks"} desc={"nao vai exitir mais cafe no mundo"} />
       <Detalhamento img={img4} nome={"jhin"} desc={"vair sofr proo 4rão"} />
       <Detalhamento img={img5} nome={"syndra"} desc={"vai tomar tanta polada que quando ver ta morto"} />
       <Detalhamento img={img6} nome={"mel"} desc={"nao da dano se reflete suas abilidades"} />
+      
       <Galeria>
       <Card img={img1}/>
       <Card img={img2}/>
@@ -49,9 +72,7 @@ function App() {
       <Card img={img6}/>
       </Galeria>
       
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
