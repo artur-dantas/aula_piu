d1fa85628e55763a1bcffc1b577876732da5f910
artur dantas de medeiros <artur.dantas@escolar.ifrn.edu.br>
2025-06-11 12:43:58 -0300

    adicionado comentario de uma outra sugestao de  alternativa

--- estu_pro_1/src/componentes/Card.jsx
+++ estu_pro_1/src/componentes/Card.jsx
@@ -49,3 +49,25 @@ export default function TarefaItem() {
     </div>
   );
 }
+
+
+{/*
+
+  import { useState } from "react"
+
+export default function Tarefa (props) {
+
+    let [concluida, setConcluida] = useState(props.concluida)
+
+    return(
+        <>
+        <h3>{props.nome}</h3>
+        <p>Descrição: {props.desc}</p>
+        <p style={{ color: concluida ? "green" : "red" }}>A Tarefa está {concluida == true ? "Concluida" : "Pendente"}</p>
+        <button onClick={() => setConcluida(!concluida)}>{concluida == false ? "Concluir" : "Reabrir"}</button>
+        <hr />
+        </>
+    )
+}
+    
+ */}
